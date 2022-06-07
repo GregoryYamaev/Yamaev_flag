33473eb09e9e0e37b08d5575e0a40895d0a7b9cf
GregoryYamaev <e_g.o.yamaev@mpt.ru>
2022-06-07 12:34:35 +0300

    JS test

---
+++ JS/assets/sj/script.js
@@ -0,0 +1,10 @@
+let name = confirm('Эта работа сильно запоздала?')
+
+function ru() {
+    if(name == true) {
+        document.location.href="./assets/sj/thefunny.html";
+    }
+    else{
+        alert('Поставлено');
+    }
+}
---
+++ JS/assets/sj/thefunny.js
@@ -0,0 +1,6 @@
+(document).ready(function() {
+    setTimeout( function(){
+        ('.numberthree').addClass('fixed');
+    },1);
+        ('.numberthree').css('position','fixed');
+    });   
